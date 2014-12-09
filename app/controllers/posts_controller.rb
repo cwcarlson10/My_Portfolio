574783587cb369a0956e9e41ace9f9963560dff1
Chris Carlson <cwcarlson10@gmail.com>
2014-12-09 12:59:38 -0700

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def index
+		@posts = Post.all
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+end
+
